445cbd4e3e470ff1f929e0dd596fddefe503a77c
adrianagers <adrianageraldine.19@gmail.com>
2020-10-23 17:49:09 -0500

    creacion del metodo update libro and genero

--- routes/book.js
+++ routes/book.js
@@ -2,5 +2,5 @@ module.exports = (app) =>{
     const book =require('../controllers/book')
     
     app.post('/book/create',book.create)
-    // app.put('/genre/update/:id',user.update)
+    app.put('/book/update/:id',book.update)
 }
